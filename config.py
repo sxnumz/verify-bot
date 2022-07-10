2e49f33b33c7f83dc0f0e5402cf737f4642aa13b
sxnumz <103958617+sxnumz@users.noreply.github.com>
2022-07-11 03:00:18 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-Bot_token = "OTg2NjgwNzI5NDM2MDQ1MzMy.GSsb4q.X4D3DOjQCvd-e6Hx2jMiWy94CTkjZgfT5v-ZrM"
+Bot_token = "OTg2NjgwNzI5NDM2MDQ1MzMy.GOL0D5.zyrLGzANZIIWSNBY2KjS7kqM8Ds_OwoWsmjgMo"
 Bot_prefix = "!"
 Verify = 986685605306056725
